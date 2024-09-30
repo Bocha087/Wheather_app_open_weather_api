47fbeb1de444a44c932e0f588596d919cde37138
Bocha087 <55623551+Bocha087@users.noreply.github.com>
2024-09-30 15:00:08 +0900

    2

--- app/src/main/java/com/example/wheather_app_open_weather_api/MainActivity.kt
+++ app/src/main/java/com/example/wheather_app_open_weather_api/MainActivity.kt
@@ -22,13 +22,17 @@ import android.Manifest
 
 
 class MainActivity : AppCompatActivity() {
-    // api id for url
+    // Ваш ключ API для доступа к OpenWeatherMap
     var api_key = "db435b3925bddf0584929aa6c7c666a3"
 
+    // Определяем переменные для интерфейса пользователя и клиента местоположения
     private lateinit var btVar1: Button
     private lateinit var textView: TextView
+    private lateinit var humidityTextView : TextView
+    //FusedLocationProviderClient — это высокоуровневый API для отслеживания местоположения, предоставляемый Google Play Services.
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    // Код запроса разрешения на доступ к местоположению
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,93 +45,111 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // link the textView in which the temperature will be displayed
         textView = findViewById(R.id.textView)
         btVar1 = findViewById(R.id.btVar1)
 
-        // create an instance of the Fused Location Provider Client
+        humidityTextView = findViewById(R.id.humidityTextView)
+
+      // Создаем экземпляр клиента для получения местоположения
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // on clicking this button function to get the coordinates will be called
+
+        // Проверяем разрешения на доступ к местоположению
         btVar1.setOnClickListener {
             checkForPermission()
         }
     }
 
+    //Метод для проверки наличия разрешений
     private fun checkForPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            // Request permissions
+            // Запрашиваем разрешения, если они не предоставлены
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE)
         } else {
-            // Permissions are already granted, obtain the location
+            //  Если разрешения уже предоставлены, получаем местоположение
             obtainLocation()
         }
     }
 
+    // Метод, который обрабатывает результат запроса разрешений
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Проверяем код запроса разрешений
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            // Если разрешение было предоставлено
             if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permission was granted, obtain the location
+                // Получаем местоположение
                 obtainLocation()
             } else {
-                // Permission denied, show a message to the user
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                // Если разрешение было отклонено, показываем сообщение
+                Toast.makeText(this, "Разрешение отклонено", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     @SuppressLint("MissingPermission")
     private fun obtainLocation() {
-        // get the last location
+        // Получаем последнее известное местоположение
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
-                // check if location is not null
+                // Проверяем, что местоположение не равно null
                 if (location != null) {
-                    // Create the URL for the current location
+                    // Формируем URL для запроса к API погоды с текущими координатами
                     val weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${api_key}"
-                    // fetch the temperature with current location
+                    // Запрашиваем температуру по текущему местоположению
                     getTemp(weatherUrl)
                 } else {
-                    Toast.makeText(this, "Unable to fetch current location.", Toast.LENGTH_SHORT).show()
+                    // Если не удалось получить местоположение, выводим сообщение
+                    Toast.makeText(this, "Не удалось получить местоположение", Toast.LENGTH_SHORT).show()
                 }
             }
+            //это метод, который используется в Android для обработки неудачных результатов асинхронных операций
             .addOnFailureListener { exception ->
+                // Если не удалось получить местоположение, показываем сообщение
                 Toast.makeText(this, "Location Permission not granted", Toast.LENGTH_SHORT).show()
             }
     }
 
     private fun getTemp(url: String) {
-        // Instantiate the RequestQueue.
+        // Создаем очередь для запросов
+        //Volley - это HTTP-библиотека, которая используется для
+        // кэширования и выполнения сетевого запроса в приложениях Android.
         val queue = Volley.newRequestQueue(this)
 
-        // Request a string response from the provided URL.
+   // Выполняем запрос к API погоды
         val stringReq = StringRequest(
             Request.Method.GET, url, { response ->
-                // Get the JSON object
+                // Получаем JSON-объект из ответа
                 val obj = JSONObject(response)
 
-                // Getting the temperature readings from response
+                // Получаем данные о температуре из объекта
                 val main: JSONObject = obj.getJSONObject("main")
                 val temperature = main.getString("temp")
                 println(temperature)
 
-                // Getting the city name
+                // Получаем данные о влажности из объекта
+                val humidity = main.getString("humidity") // Получаем влажность
+                println(humidity)
+
+                // Получаем название города из объекта
                 val city = obj.getString("name")
                 println(city)
 
-                // Set the temperature and the city name using getString() function
+                // Устанавливаем текст в textView, показывая температуру и название города
                 textView.text = "${temperature} Градусов по цельсию в ${city}"
                 System.out.println(obj.toString())
-            },
-            // In case of any error
-            { textView.text = "That didn't work!" })
 
-        // Add the request to the RequestQueue.
+                // Устанавливаем влажность в humidityTextView
+                humidityTextView.text = "Влажность: $humidity%"
+            },
+            //  В случае ошибки показываем сообщение
+            { textView.text = "Ошибка!" })
+
+        // Добавляем запрос в очередь
         queue.add(stringReq)
     }
 }
